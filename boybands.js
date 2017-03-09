c16323cbe6c2165214344824f50eee9b719367c3
Tamela Lerma <tlerma83@gmail.com>
2017-03-09 09:20:40 -0600

    Better JS

--- boybands.js
+++ boybands.js
@@ -11,16 +11,18 @@ var bandElement= document.getElementById("boy-bands");
 
 var veggieElement = document.getElementById("vegetables");
 
-// Start looping
-for (var i = 0; i < loopCount; i++) {
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker++) {
 
-    currentBand = bands[i];
-    console.log(currentBand);
-    bandElement.innerHTML += "<p>" + currentBand + "</p>"; 
+  // Add the band names into the correct <div>
+    currentBand = bands[loopTracker];
+    var paragraphs = document.createElement("p");
+    paragraphs.innerHTML = currentBand;
+    bandElement.appendChild(paragraphs);
     
-    currentVeggie = vegetables[i];
-    console.log(currentVeggie);
-    veggieElement.innerHTML += "<p>" + currentVeggie + "</p>";
-}
+    currentVeggie = vegetables[loopTracker];
+    var paragraph2 = document.createElement("p");
+    paragraph2.innerHTML = currentVeggie;
+    veggieElement.appendChild(paragraph2);
 
+}
 
